1c139b0c6898c29b39f0e1c6ff47d82b31fe4f33
Rafael Martinez <rafaelvady.q@gmail.com>
2024-11-19 04:18:59 -0600

    add profile form

---
+++ app/screens/protected/ProfileInfo.tsx
@@ -0,0 +1,187 @@
+import React from 'react'
+import { ScrollView, StyleSheet, Text, View } from 'react-native'
+
+import TextField from '../../ui/Forms/TextField'
+import { Picker } from '@react-native-picker/picker'
+import { Formik } from 'formik'
+import { IGetGeneralUser } from 'interfaces/CreateUser.interface'
+import * as Yup from 'yup'
+
+import Button from '@ui/components/Button'
+
+import colors from '@config/theme/colors'
+
+const SignupSchema = Yup.object().shape({
+  alias: Yup.string().min(4, 'Muy corto!').required('El pronombre es requerido'),
+  email: Yup.string().email('Correo inválido').required('El correo es requerido'),
+  day: Yup.number().max(31, 'Solo días calendario válidos').required('El día es requerido'),
+  month: Yup.number().min(1).max(12).required('El mes es requerido'),
+  year: Yup.number()
+    .min(1950, 'El año no puede ser menor a 1950')
+    .max(new Date().getFullYear(), `El año no debe ser mayor a ${new Date().getFullYear()}`)
+    .required('El año es requerido')
+})
+
+interface Props {
+  onSubmit: Function
+  userInformation: IGetGeneralUser | null
+}
+
+export function ProfileInfo({ onSubmit, userInformation }: Props) {
+  const birthDateParts = userInformation?.data?.birth_date ? userInformation.data.birth_date.split('/') : ['', '', '']
+
+  const months = [
+    { text: 'Seleccione un mes', value: null },
+    { text: 'Enero', value: 1 },
+    { text: 'Febrero', value: 2 },
+    { text: 'Marzo', value: 3 },
+    { text: 'Abril', value: 4 },
+    { text: 'Mayo', value: 5 },
+    { text: 'Junio', value: 6 },
+    { text: 'Julio', value: 7 },
+    { text: 'Agosto', value: 8 },
+    { text: 'Septiembre', value: 9 },
+    { text: 'Octubre', value: 10 },
+    { text: 'Noviembre', value: 11 },
+    { text: 'Diciembre', value: 12 }
+  ]
+
+  return (
+    <ScrollView style={styles.scrollView}>
+      <Formik
+        initialValues={{
+          alias: userInformation?.data?.alias || '',
+          email: userInformation?.data?.email || '',
+          day: birthDateParts[1] || '',
+          month: birthDateParts[0] || null,
+          year: birthDateParts[2] || ''
+        }}
+        validationSchema={SignupSchema}
+        onSubmit={values => {
+          const dataToSend = {
+            alias: values.alias,
+            email: values.email,
+            birth_date: `${values.month}/${values.day}/${values.year}`
+          }
+          onSubmit(dataToSend)
+        }}
+      >
+        {({ handleChange, handleBlur, handleSubmit, values, setFieldValue, errors, touched }: any) => (
+          <View style={styles.formContainer}>
+            <View style={styles.spacing}>
+              <Text style={styles.label}>Nombre / Usuario </Text>
+              <TextField
+                onChangeText={handleChange('alias')}
+                onBlur={handleBlur('alias')}
+                value={values.alias}
+                placeholder='Pronombre'
+              />
+              {errors.alias && touched.alias && <Text style={styles.errorText}>{errors.alias}</Text>}
+            </View>
+
+            <View style={styles.spacing}>
+              <Text style={styles.label}>Correo electrónico</Text>
+              <TextField
+                onChangeText={handleChange('email')}
+                onBlur={handleBlur('email')}
+                value={values.email}
+                placeholder='Correo electrónico'
+              />
+              {errors.email && touched.email && <Text style={styles.errorText}>{errors.email}</Text>}
+            </View>
+
+            <View style={styles.spacing}>
+              <Text style={styles.label}>Fecha de nacimiento</Text>
+              <View style={styles.dateOfBirthContainer}>
+                <View style={styles.pickerWrapper}>
+                  <TextField
+                    keyBoardType='number-pad'
+                    onChangeText={handleChange('day')}
+                    onBlur={handleBlur('day')}
+                    value={values.day}
+                    placeholder='Día'
+                  />
+                </View>
+                <View style={styles.pickerWrapper}>
+                  <Picker
+                    selectedValue={values.month}
+                    style={styles.picker}
+                    onValueChange={itemValue => setFieldValue('month', itemValue)}
+                  >
+                    {months.map(el => (
+                      <Picker.Item key={el.value} label={el.text} value={el.value} />
+                    ))}
+                  </Picker>
+                </View>
+                <View style={styles.pickerWrapper}>
+                  <TextField
+                    keyBoardType='number-pad'
+                    onChangeText={handleChange('year')}
+                    onBlur={handleBlur('year')}
+                    value={values.year}
+                    placeholder='Año'
+                  />
+                </View>
+              </View>
+              {errors.day && touched.day && <Text style={styles.errorText}>{errors.day}</Text>}
+              {errors.month && touched.month && <Text style={styles.errorText}>{errors.month}</Text>}
+              {errors.year && touched.year && <Text style={styles.errorText}>{errors.year}</Text>}
+            </View>
+
+            <Button appearance='filled' color={colors.primary} handleClick={handleSubmit} rounded>
+              CONFIRMAR
+            </Button>
+          </View>
+        )}
+      </Formik>
+    </ScrollView>
+  )
+}
+
+const styles = StyleSheet.create({
+  scrollView: {
+    flexGrow: 1,
+    width: '100%'
+  },
+  formContainer: {
+    padding: 20,
+    alignItems: 'center',
+    flex: 1,
+    backgroundColor: '#F8F8F8',
+    borderTopLeftRadius: 30,
+    borderTopRightRadius: 30,
+    justifyContent: 'space-evenly',
+    paddingBottom: 20
+  },
+  label: {
+    fontSize: 14,
+    fontWeight: 'bold',
+    marginBottom: 10,
+    color: colors.primaryTextColor,
+    textAlign: 'left'
+  },
+  spacing: {
+    width: '90%',
+    marginBottom: 35
+  },
+  pickerWrapper: {
+    flex: 1,
+    marginHorizontal: 5
+  },
+  dateOfBirthContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    width: '100%'
+  },
+  picker: {
+    flex: 1,
+    backgroundColor: '#E0E0E0',
+    borderRadius: 10,
+    color: colors.primary
+  },
+  errorText: {
+    color: 'red',
+    fontSize: 12,
+    marginTop: 5
+  }
+})
--- app/screens/protected/ProfileInformation.tsx
+++ app/screens/protected/ProfileInformation.tsx
@@ -1,156 +1,167 @@
-import React, { useEffect, useState } from 'react';
-import { View, Text, Image, StyleSheet, TouchableOpacity, ActivityIndicator, Alert } from 'react-native';
-import Icon from 'react-native-vector-icons/MaterialIcons';
-import AsyncStorage from '@react-native-async-storage/async-storage';
-import { StackNavigationProp } from '@react-navigation/stack';
-import { RootStackParamList } from '@screens/RegisterScreen';
-import { IGetGeneralUser } from 'interfaces/CreateUser.interface';
-import { getUserInfo } from '@services/GetUserInfo.service';
+import React, { useEffect, useState } from 'react'
+import { ActivityIndicator, Alert, Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
 
-type ProfileScreenNavigationProp = StackNavigationProp<RootStackParamList, 'Login'>;
+import AsyncStorage from '@react-native-async-storage/async-storage'
+import { StackNavigationProp } from '@react-navigation/stack'
+import { IGetGeneralUser } from 'interfaces/CreateUser.interface'
+import Icon from 'react-native-vector-icons/MaterialIcons'
+
+import { getUserInfo } from '@services/GetUserInfo.service'
+
+import { RootStackParamList } from '@screens/RegisterScreen'
+
+import { ProfileInfo } from './ProfileInfo'
+
+type ProfileScreenNavigationProp = StackNavigationProp<RootStackParamList, 'Login'>
 
 interface Props {
-  navigation: ProfileScreenNavigationProp;
+  navigation: ProfileScreenNavigationProp
 }
 
 const ProfileInformation = ({ navigation }: Props) => {
-  const [userInformation, setUserInformation] = useState<IGetGeneralUser | null>(null);
-  const [loading, setLoading] = useState<boolean>(true);
+  const [userInformation, setUserInformation] = useState<IGetGeneralUser | null>(null)
+  const [loading, setLoading] = useState<boolean>(true)
+  const [showUserForm, setShowUserForm] = useState<boolean>(false)
 
-  const handleEdit = () => console.log('Edit Profile');
+  const handleEdit = () => console.log('Edit Profile')
   const handleLogout = async () => {
     try {
-      await AsyncStorage.removeItem('jwt'); 
-      navigation.navigate('Login'); 
+      await AsyncStorage.removeItem('jwt')
+      navigation.navigate('Login')
     } catch (error) {
-      Alert.alert('Error', 'Hubo un problema al cerrar sesión. Intenta de nuevo.');
-      console.error('Logout error:', error);
+      Alert.alert('Error', 'Hubo un problema al cerrar sesión. Intenta de nuevo.')
+      console.error('Logout error:', error)
     }
-  };
+  }
 
-  const handlePersonalInfo = () => console.log('Go to Personal Info');
-  const handleCalendar = () => console.log('Go to Calendar');
-  const handleProtocols = () => console.log('Go to Protocols');
+  const handlePersonalInfo = () => setShowUserForm(true)
+  const handleCalendar = () => console.log('Go to Calendar')
+  const handleProtocols = () => console.log('Go to Protocols')
 
   const getUserData = async () => {
     try {
-      const userData = await getUserInfo();
-      setUserInformation(userData);
+      const userData = await getUserInfo()
+      setUserInformation(userData)
     } catch (error) {
-      console.error('Error fetching user info:', error);
-      Alert.alert('Error', 'No se pudieron cargar los datos del usuario.');
+      console.error('Error fetching user info:', error)
+      Alert.alert('Error', 'No se pudieron cargar los datos del usuario.')
     } finally {
-      setLoading(false);
+      setLoading(false)
     }
-  };
+  }
 
   useEffect(() => {
-    getUserData();
-  }, []);
+    getUserData()
+  }, [])
 
   if (loading) {
     return (
       <View style={styles.loaderContainer}>
-        <ActivityIndicator size="large" color="#6A1B9A" />
+        <ActivityIndicator size='large' color='#6A1B9A' />
         <Text style={styles.loadingText}>Cargando...</Text>
       </View>
-    );
+    )
   }
 
   return (
     <View style={styles.container}>
       <View style={styles.header}>
         <TouchableOpacity style={styles.backButton}>
-          <Icon name="arrow-back" size={24} color="white" />
+          <Icon name='arrow-back' size={24} color='white' />
         </TouchableOpacity>
         <Text style={styles.title}>Mi perfil</Text>
         <View style={styles.imageContainer}>
           <Image
             source={{
-              uri: userInformation?.data?.avatar ?? 'https://via.placeholder.com/100',
+              uri: userInformation?.data?.avatar ?? 'https://via.placeholder.com/100'
             }}
             style={styles.profileImage}
           />
           <TouchableOpacity style={styles.editIcon} onPress={handleEdit}>
-            <Icon name="camera-alt" size={18} color="white" />
+            <Icon name='camera-alt' size={18} color='white' />
           </TouchableOpacity>
         </View>
         <Text style={styles.name}>{userInformation?.data?.name ?? 'No name available'}</Text>
         <Text style={styles.email}>{userInformation?.data?.email ?? 'No email available'}</Text>
       </View>
-
       <View style={styles.body}>
-        <TouchableOpacity style={styles.button} onPress={handlePersonalInfo}>
-          <Icon name="person" size={45} color="#6A1B9A" />
-          <Text style={styles.buttonText}>Información personal</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.button} onPress={handleCalendar}>
-          <Icon name="calendar-today" size={45} color="#6A1B9A" />
-          <Text style={styles.buttonText}>Calendario Menstrual</Text>
-        </TouchableOpacity>
+        {!showUserForm ? (
+          <>
+            <TouchableOpacity style={styles.button} onPress={handlePersonalInfo}>
+              <Icon name='person' size={45} color='#6A1B9A' />
+              <Text style={styles.buttonText}>Información personal</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.button} onPress={handleCalendar}>
+              <Icon name='calendar-today' size={45} color='#6A1B9A' />
+              <Text style={styles.buttonText}>Calendario Menstrual</Text>
+            </TouchableOpacity>
 
-        <View style={styles.footer}>
-          <View>
-            <TouchableOpacity style={styles.footerButton} onPress={handleProtocols}>
-              <Icon name="security" size={20} color="#6A1B9A" />
-              <Text style={styles.footerText}>Protocolos de seguridad</Text>
-            </TouchableOpacity>
-          </View>
-          <View>
-            <TouchableOpacity style={styles.footerButton} onPress={handleLogout}>
-              <Icon name="logout" size={20} color="#6A1B9A" />
-              <Text style={styles.footerText}>Cerrar Sesión</Text>
-            </TouchableOpacity>
-          </View>
-        </View>
+            <View style={styles.footer}>
+              <View>
+                <TouchableOpacity style={styles.footerButton} onPress={handleProtocols}>
+                  <Icon name='security' size={20} color='#6A1B9A' />
+                  <Text style={styles.footerText}>Protocolos de seguridad</Text>
+                </TouchableOpacity>
+              </View>
+              <View>
+                <TouchableOpacity style={styles.footerButton} onPress={handleLogout}>
+                  <Icon name='logout' size={20} color='#6A1B9A' />
+                  <Text style={styles.footerText}>Cerrar Sesión</Text>
+                </TouchableOpacity>
+              </View>
+            </View>
+          </>
+        ) : (
+          <ProfileInfo userInformation={userInformation} onSubmit={() => setShowUserForm(false)} />
+        )}
       </View>
     </View>
-  );
-};
+  )
+}
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#6A1B9A',
+    backgroundColor: '#6A1B9A'
   },
   loaderContainer: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#FFF',
+    backgroundColor: '#FFF'
   },
   loadingText: {
     marginTop: 10,
     color: '#6A1B9A',
     fontSize: 16,
-    fontWeight: 'bold',
+    fontWeight: 'bold'
   },
   header: {
     alignItems: 'center',
     padding: 20,
-    backgroundColor: '#6A1B9A',
+    backgroundColor: '#6A1B9A'
   },
   backButton: {
     position: 'absolute',
     top: 20,
-    left: 20,
+    left: 20
   },
   title: {
     color: 'white',
     fontSize: 20,
     fontWeight: 'bold',
-    marginTop: 10,
+    marginTop: 10
   },
   imageContainer: {
     marginTop: 20,
-    alignItems: 'center',
+    alignItems: 'center'
   },
   profileImage: {
     width: 100,
     height: 100,
     borderRadius: 50,
     borderWidth: 3,
-    borderColor: '#FFF',
+    borderColor: '#FFF'
   },
   editIcon: {
     position: 'absolute',
@@ -158,18 +169,18 @@ const styles = StyleSheet.create({
     right: -10,
     backgroundColor: '#6A1B9A',
     padding: 5,
-    borderRadius: 15,
+    borderRadius: 15
   },
   name: {
     fontSize: 18,
     color: '#FFF',
     marginTop: 10,
-    fontWeight: 'bold',
+    fontWeight: 'bold'
   },
   email: {
     fontSize: 14,
     color: '#FFF',
-    marginTop: 5,
+    marginTop: 5
   },
   body: {
     flex: 1,
@@ -177,8 +188,8 @@ const styles = StyleSheet.create({
     borderTopLeftRadius: 30,
     borderTopRightRadius: 30,
     alignItems: 'center',
-    justifyContent: 'space-evenly', 
-    paddingBottom: 20, 
+    justifyContent: 'space-evenly',
+    paddingBottom: 20
   },
   button: {
     backgroundColor: '#F3E5F5',
@@ -188,13 +199,13 @@ const styles = StyleSheet.create({
     padding: 15,
     borderRadius: 10,
     marginVertical: 10,
-    width: '35%',
+    width: '35%'
   },
   buttonText: {
-    textAlign:'center',
+    textAlign: 'center',
     fontSize: 16,
     color: '#6A1B9A',
-    fontWeight: 'bold',
+    fontWeight: 'bold'
   },
   footer: {
     flexDirection: 'row',
@@ -203,18 +214,18 @@ const styles = StyleSheet.create({
     width: '100%',
     paddingVertical: 10,
     backgroundColor: '#F8F8F8',
-    marginBottom:30
+    marginBottom: 30
   },
   footerButton: {
-    width:'50%',
+    width: '50%',
     flexDirection: 'row',
-    alignItems: 'center',
+    alignItems: 'center'
   },
   footerText: {
     marginLeft: 5,
     fontSize: 14,
-    color: '#6A1B9A',
-  },
-});
+    color: '#6A1B9A'
+  }
+})
 
-export default ProfileInformation;
+export default ProfileInformation
